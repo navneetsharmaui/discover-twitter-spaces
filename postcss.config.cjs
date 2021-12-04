89fd086cbefc8485e0c493080f80b484089b29a7
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-04 18:43:39 +0530

    Added the tailwindcss configuration

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,4 @@
+const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
@@ -6,8 +7,10 @@ const dev = mode === 'development';
 
 const config = {
 	plugins: [
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		tailwindcss(),
+		//But others, like autoprefixer, need to run after,
 		autoprefixer(),
-
 		!dev &&
 			cssnano({
 				preset: 'default'
