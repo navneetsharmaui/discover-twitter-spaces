93ebc599d79a43703f22227893aeab87bef1bb92
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-06 15:51:49 +0530

    Added the spaces category data

---
+++ src/lib/data/spaces-categories.ts
@@ -0,0 +1,60 @@
+import type { ISpacesCategories } from '$lib/models/interfaces/ispaces-categories.interface';
+
+export const SPACE_CATEGORIES: ISpacesCategories[] = [
+	{
+		id: 'category',
+		label: 'Select Category',
+	},
+	{
+		id: 'nft',
+		label: 'NFT',
+	},
+	{
+		id: 'tech',
+		label: 'Tech',
+	},
+	{
+		id: 'web',
+		label: 'Web',
+	},
+	{
+		id: 'rust',
+		label: 'Rust',
+	},
+	{
+		id: 'web3',
+		label: 'Web3',
+	},
+	{
+		id: 'crypto',
+		label: 'Crypto',
+	},
+	{
+		id: 'bitcoin',
+		label: 'Bitcoin',
+	},
+	{
+		id: 'ethereum',
+		label: 'Ethereum',
+	},
+	{
+		id: 'world',
+		label: 'World',
+	},
+	{
+		id: 'health',
+		label: 'Health',
+	},
+	{
+		id: 'coding',
+		label: 'Coding',
+	},
+	{
+		id: 'programming',
+		label: 'Programming',
+	},
+	{
+		id: 'politics',
+		label: 'Politics',
+	},
+];
