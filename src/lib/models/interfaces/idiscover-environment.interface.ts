1f5458d3124641929bd7893502ccbf149e38129c
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-08 19:23:45 +0530

    Added the apiconfig field to the IDiscoverEnvironmentConfig

--- src/lib/models/interfaces/idiscover-environment.interface.ts
+++ src/lib/models/interfaces/idiscover-environment.interface.ts
@@ -1,4 +1,5 @@
 import type { DISCOVER_DATA_ENPOINT_CONFIG } from '../types/discover-endpoits.type';
+import type { IDiscoverDBConfig } from './idiscover-db-config.interface';
 import type { IEnvironmentConfig } from './ienvironment.interface';
 import type { IGithubConfig } from './igithub-config.interface';
 import type { ITwitterConfig } from './itwitter-config.interface';
@@ -6,4 +7,5 @@ import type { ITwitterConfig } from './itwitter-config.interface';
 export interface IDiscoverEnvironmentConfig<T = DISCOVER_DATA_ENPOINT_CONFIG> extends IEnvironmentConfig {
 	twitterConfig: Partial<ITwitterConfig>;
 	githubConfig: Partial<IGithubConfig>;
+	apiConfig?: Partial<IDiscoverDBConfig<T>>;
 }
