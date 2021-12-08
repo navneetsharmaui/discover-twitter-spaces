2ac24c5dd6c86c3be0aabaf55a8a82f4ae281069
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-08 21:29:59 +0530

    Updated the vercel function

--- api/discover-spaces.ts
+++ api/discover-spaces.ts
@@ -4,7 +4,6 @@
  */
 
 import dotenv from 'dotenv';
-import isomorphicFetch from 'isomorphic-fetch';
 
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
@@ -15,35 +14,11 @@ export default async (
 	response: VercelResponse,
 ): Promise<VercelResponse> => {
 	try {
-		const TWITTER_TOKEN = process.env.DISCOVER_TWITTER_TOKEN;
-
-		const TWITTER_BASE_API_URL = process.env.DISCOVER_TWITTER_BASE_API_URL;
-
-		const headers = new Headers();
-
-		headers.append('Authorization', `Bearer ${TWITTER_TOKEN}`);
-
-		const httpRequestOptions = {
-			headers,
-			method: 'GET',
-		};
-
 		const { search } = request.query;
 
 		const searchQuery = search ? search : 'Web';
 
-		const spacesSearchQueryParameters =
-			'state=all&topic.fields=id,name,description&space.fields=host_ids,created_at,creator_id,id,lang,invited_user_ids,participant_count,speaker_ids,started_at,ended_at,topic_ids,state,title,updated_at,scheduled_start,is_ticketed&expansions=invited_user_ids,speaker_ids,creator_id,host_ids&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld';
-
-		const url = `${TWITTER_BASE_API_URL}/spaces/search?query=${searchQuery}&${spacesSearchQueryParameters}`;
-
-		const twitterSpacesApiResponse = await isomorphicFetch(url, httpRequestOptions);
-
-		return !twitterSpacesApiResponse.ok
-			? response
-					.status(twitterSpacesApiResponse.status)
-					.send(twitterSpacesApiResponse.statusText)
-			: response.status(200).send(JSON.stringify(await twitterSpacesApiResponse.json()));
+		return response.status(200).send({ message: `Hello ${searchQuery}` });
 	} catch (error) {
 		return response.status(500).send({
 			error: JSON.stringify({
