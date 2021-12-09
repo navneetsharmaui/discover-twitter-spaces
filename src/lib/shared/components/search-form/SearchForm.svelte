64ff8296e59a48a0955198d9950aaa1906db7677
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-09 14:13:16 +0530

    Created a separate component for search form

---
+++ src/lib/shared/components/search-form/SearchForm.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { SPACE_CATEGORIES } from '$lib/data/spaces-categories';
+
+	const dispatch = createEventDispatcher();
+	let searchValue = '';
+	let selectedValue = 'category';
+
+	const handleSearch = async (): Promise<void> => {
+		if (searchValue.length >= 3) {
+			selectedValue = 'category';
+		}
+	};
+
+	const handlerSelection = async (): Promise<void> => {
+		if (
+			selectedValue.length >= 3 &&
+			selectedValue !== searchValue &&
+			selectedValue !== 'category'
+		) {
+			searchValue = '';
+		}
+	};
+
+	const onDispatchSearchField = (input: string): void => {
+		dispatch('submitSearchField', input);
+	};
+
+	const onSubmit = async (): Promise<void> => {
+		if (
+			selectedValue.length >= 3 &&
+			selectedValue !== searchValue &&
+			selectedValue !== 'category'
+		) {
+			searchValue = '';
+			onDispatchSearchField(selectedValue);
+		} else if (searchValue.length >= 3) {
+			selectedValue = 'category';
+			onDispatchSearchField(searchValue);
+		}
+	};
+</script>
+
+<div class="flex flex-col mb-4 w-full">
+	<div class="relative w-full mb-2">
+		<input
+			bind:value="{searchValue}"
+			on:input="{() => handleSearch()}"
+			aria-label="Search articles"
+			type="text"
+			placeholder="Search Twitter Spaces"
+			class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-yellow-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+		/>
+		<svg
+			class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="{2}"
+				d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+		</svg>
+	</div>
+
+	<div class="w-full flex flex-row justify-center items-center text-black dark:text-white mb-2">
+		<p> -- OR -- </p>
+	</div>
+
+	<div class="w-full mb-4">
+		<label class="block" for="category">
+			<select
+				class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-yellow-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+				name="category"
+				id="category"
+				bind:value="{selectedValue}"
+				on:change="{() => handlerSelection()}"
+			>
+				{#each SPACE_CATEGORIES as category, index (category.id)}
+					<option value="{category.id}">{category.label}</option>
+				{/each}
+			</select>
+		</label>
+	</div>
+
+	<div class="w-full mb-4">
+		<button
+			on:click="{() => onSubmit()}"
+			type="button"
+			class="w-full bg-gray-300 hover:bg-gray-400 text-black active:bg-gray-400 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+		>
+			Submit
+		</button>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,13 +16,13 @@
 <script lang="ts">
 	// Start: Imports
 	import HeadTags from '$components/head-tags/HeadTags.svelte';
-	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
-	import type { ITwitterSpace } from '$lib/models/interfaces/itwitter-space.interface';
 	import SpaceCard from '$lib/shared/ui/components/space-card/SpaceCard.svelte';
 	import GhostSpaceCard from '$lib/shared/ui/components/ghost-space-card/GhostSpaceCard.svelte';
+	import SearchForm from '$lib/shared/components/search-form/SearchForm.svelte';
+	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
+	import type { ITwitterSpace } from '$lib/models/interfaces/itwitter-space.interface';
 	import { api } from '$lib/core/services/https/_api';
 	import { spacesStore } from '$stores/spaces-store';
-	import { SPACE_CATEGORIES } from '$lib/data/spaces-categories';
 
 	// Exports
 	// Start: Local component properties
@@ -45,8 +45,7 @@
 		],
 	};
 
-	let searchValue = '';
-	let selectedValue = 'category';
+	let searchedField = '';
 
 	// End: Local component properties
 
@@ -61,26 +60,13 @@
 			: await api(`/api/spaces.json?search=${value}`);
 	};
 
-	const setTwitterSpaces = async (input: string): Promise<void> => {
-		const response = await fetchSpaces(input);
+	const setTwitterSpaces = async (input: CustomEvent<string>): Promise<void> => {
+		isLoading = true;
+		const response = await fetchSpaces(input.detail);
 		twitterSpaces = response.body as unknown as ITwitterSpace[];
 		spacesStore.set(twitterSpaces);
+		searchedField = input.detail.toUpperCase();
 		isLoading = false;
-	};
-	const handleSearch = async (input: string): Promise<void> => {
-		if (input.length >= 3) {
-			isLoading = true;
-			selectedValue = 'category';
-			setTwitterSpaces(input);
-		}
-	};
-
-	const handleSelect = async (input: string): Promise<void> => {
-		if (input.length >= 3 && input !== searchValue && input !== 'category') {
-			isLoading = true;
-			searchValue = '';
-			setTwitterSpaces(input);
-		}
 	};
 
 	// End: Local component methods
@@ -99,64 +85,14 @@
 		Search the twitter space for your interests. You can also search for people and topics. You
 		can also search for spaces by category.
 	</p>
-	<div class="flex flex-col mb-4 w-full">
-		<div class="relative w-full mb-2">
-			<input
-				bind:value="{searchValue}"
-				on:input="{() => handleSearch(searchValue)}"
-				aria-label="Search articles"
-				type="text"
-				placeholder="Search articles"
-				class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-yellow-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-			/>
-			<svg
-				class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="{2}"
-					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-			</svg>
-		</div>
-
-		<div class="w-full flex flex-row justify-center items-center mb-2">
-			<p> -- OR -- </p>
-		</div>
-
-		<div class="w-full mb-4">
-			<label class="block" for="category">
-				<select
-					class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-yellow-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-					name="category"
-					id="category"
-					bind:value="{selectedValue}"
-					on:change="{() => handleSelect(selectedValue)}"
-				>
-					{#each SPACE_CATEGORIES as category, index (category.id)}
-						<option value="{category.id}">{category.label}</option>
-					{/each}
-				</select>
-			</label>
-		</div>
-
-		<div class="w-full mb-4">
-			<button
-				type="button"
-				class="w-full bg-gray-300 hover:bg-gray-500 text-gray-900 active:bg-gray-400 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-			>
-				Submit
-			</button>
-		</div>
-	</div>
+	<SearchForm on:submitSearchField="{(e) => setTwitterSpaces(e)}" />
 	<div class="border-t border-gray-400 w-full my-2"> </div>
 	<div class="w-full">
 		<p class="text-gray-900 dark:text-gray-100 mx-1">
-			<small>About {$spacesStore.length} results</small>
+			<small
+				>About {$spacesStore.length}
+				{searchedField ? `results for ${searchedField}.` : 'results.'}</small
+			>
 		</p>
 	</div>
 	<div class="flex flex-col w-full mt-5 gap-5">
