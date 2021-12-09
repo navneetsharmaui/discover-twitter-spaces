21764b694df97585d9dfec484d9654ed9ae3b07a
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-09 20:21:03 +0530

    Updated the header nvigation style

--- src/lib/shared/ui/components/navigation-bar/NavigationBar.svelte
+++ src/lib/shared/ui/components/navigation-bar/NavigationBar.svelte
@@ -45,7 +45,7 @@
 				<a
 					sveltekit:prefetch
 					href="{navLink.path}"
-					class="p-1.5 text-gray-900 sm:p-2.5 dark:text-gray-100 hover:border-b-2 hover:border-gray-800 dark:hover:border-gray-100"
+					class="p-1.5 text-sm text-gray-900 sm:p-2.5 dark:text-gray-100 hover:border-b-2 hover:border-gray-800 dark:hover:border-gray-100"
 					class:nav-active-route="{$page.path === navLink.path ||
 						$page.path === `${navLink.path}/`}"
 					class:nav-inactive-route="{$page.path !== navLink.path &&
@@ -55,13 +55,13 @@
 				</a>
 			{/each}
 		</div>
-		<div class="flex flex-row gap-5">
+		<div class="flex flex-row gap-5 items-center">
 			<ExternalLink
 				href="{twitterLink}"
 				cssClasses="inline-flex text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-400"
 				ariaLabel="{`Checkout my Twitter`}"
 			>
-				<Icon data="{faTwitter}" scale="{1.5}" />
+				<Icon data="{faTwitter}" scale="{1.5}" class="h-5 w-5" />
 			</ExternalLink>
 
 			<ExternalLink
@@ -69,32 +69,31 @@
 				cssClasses="inline-flex text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-400"
 				ariaLabel="{`Checkout on GitHub`}"
 			>
-				<Icon data="{faGithub}" scale="{1.5}" />
+				<Icon data="{faGithub}" scale="{1.5}" class="h-5 w-5" />
 			</ExternalLink>
+
+			{#if useThemeModeButton}
+				<button
+					on:click="{() => toggleTheme()}"
+					aria-label="Toggle Dark Mode"
+					type="button"
+					class="{'w-7 h-7 bg-yellow-50 rounded-full dark:bg-gray-800 filter shadow dark:shadow-dark dark:hover:shadow-dark-lg hover:border hover:border-gray-500 dark:hover:border-gray-300'}"
+				>
+					{#if dark}
+						<Icon
+							data="{faSun}"
+							class="{'h-2 w-2 text-xs text-gray-700 dark:text-gray-100'}"
+							scale="{1.5}"
+						/>
+					{:else}
+						<Icon
+							data="{faMoon}"
+							class="{'h-2 w-2 text-xs text-gray-700 dark:text-gray-100'}"
+							scale="{1.5}"
+						/>
+					{/if}
+				</button>
+			{/if}
 		</div>
 	</nav>
-	{#if useThemeModeButton}
-		<button
-			on:click="{() => toggleTheme()}"
-			aria-label="Toggle Dark Mode"
-			type="button"
-			class="{useTitleAndLogo
-				? 'absolute top-1 right-1 md:top-2 md:right-2 w-8 h-8 bg-yellow-50 rounded-full dark:bg-gray-800 filter shadow hover:shadow-md dark:shadow-dark dark:hover:shadow-dark-lg hover:border-2 hover:border-gray-500 dark:hover:border-gray-300 z-10'
-				: 'w-8 h-8 bg-yellow-50 rounded-full dark:bg-gray-800 filter shadow hover:shadow-md dark:shadow-dark dark:hover:shadow-dark-lg hover:border-2 hover:border-gray-500 dark:hover:border-gray-300 z-10'}"
-		>
-			{#if dark}
-				<Icon
-					data="{faSun}"
-					class="{'h-3 w-3 text-xs text-gray-700 dark:text-gray-100'}"
-					scale="{1.5}"
-				/>
-			{:else}
-				<Icon
-					data="{faMoon}"
-					class="{'h-3 w-3 text-xs text-gray-700 dark:text-gray-100'}"
-					scale="{1.5}"
-				/>
-			{/if}
-		</button>
-	{/if}
 </header>
