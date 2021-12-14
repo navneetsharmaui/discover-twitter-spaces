0cb819949b93231a741f0afee29c2a33b5e24f3a
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-14 16:47:47 +0530

    Removed the redundant code and component

--- src/lib/shared/ui/components/header/Header.svelte
+++ src/lib/shared/ui/components/header/Header.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	// Components
-	import { NavigationProgressBar } from '$ui/components/navigation-progress-bar';
 	import { NavigationBar } from '$ui/components/navigation-bar';
 </script>
-
-<!-- Start: Navigation Progress bar -->
-<NavigationProgressBar />
-<!-- End: Navigation Progress bar -->
 
 <!-- Start: Navigation bar -->
 <NavigationBar />
--- src/lib/shared/ui/components/navigation-progress-bar/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NavigationProgressBar } from './NavigationProgressBar.svelte';
