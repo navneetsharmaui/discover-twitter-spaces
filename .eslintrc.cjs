010e41a9a69ceb359b713cda9c42c61ae8020c49
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-20 17:12:40 +0530

    Removed the storybook eslint plugin reference from the eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:storybook/recommended',
 		'prettier',
 	],
 	plugins: ['svelte3', '@typescript-eslint'],
