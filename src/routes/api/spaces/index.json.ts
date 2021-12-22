8c0a696fc8333a98990bfac0efede176d068b634
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-22 19:01:31 +0530

    Added code to check the total api response time

--- src/routes/api/spaces/index.json.ts
+++ src/routes/api/spaces/index.json.ts
@@ -1,8 +1,13 @@
+import { Logger, LoggerUtils } from '$lib/utils/_logger';
 import type { RequestHandler } from '@sveltejs/kit';
+import { performance } from 'perf_hooks';
 import { TwitterSpacesAPIService } from './_twitter-spaces-api.service';
 
 export const get: RequestHandler = async (request) => {
 	try {
+		const logger: Logger = LoggerUtils.getInstance('spaced-api');
+
+		const start = performance.now();
 		const search = request.query.get('search');
 
 		const searchQuery = search ? search : 'Web';
@@ -11,16 +16,22 @@ export const get: RequestHandler = async (request) => {
 		const twitterSpacesApiResponse = await twitterSpacesAPIService.getSpacesFromCache(
 			searchQuery,
 		);
+		logger.debug('Time Elapsed till cache: ', (performance.now() - start) / 1000);
 
-		return twitterSpacesApiResponse && twitterSpacesApiResponse.length > 0
-			? {
-					status: 200,
-					headers: {
-						'Cache-Control': 'public, s-maxage=1200, stale-while-revalidate=600',
-					},
-					body: JSON.stringify(twitterSpacesApiResponse),
-			  }
-			: await twitterSpacesAPIService.getSpacesFromAPI(searchQuery);
+		if (twitterSpacesApiResponse && twitterSpacesApiResponse.length > 0) {
+			logger.debug('Total elapsed time: ', (performance.now() - start) / 1000);
+			return {
+				status: 200,
+				headers: {
+					'Cache-Control': 'public, s-maxage=1200, stale-while-revalidate=600',
+				},
+				body: JSON.stringify(twitterSpacesApiResponse),
+			};
+		} else {
+			const response = await twitterSpacesAPIService.getSpacesFromAPI(searchQuery);
+			logger.debug('Total elapsed time: ', (performance.now() - start) / 1000);
+			return response;
+		}
 	} catch (error) {
 		return {
 			status: 500,
