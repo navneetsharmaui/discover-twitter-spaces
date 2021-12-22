7ac70ec684d73d61051f1721b7f699bc52f8fe14
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-22 17:11:19 +0530

    Updated the config with new redis client class

--- src/lib/core/config/_redis.ts
+++ src/lib/core/config/_redis.ts
@@ -1,5 +1,20 @@
 import Redis from 'ioredis';
 
-const connectionString = process.env['DISCOVER_REDIS_CONNECTION'];
+import { RedisClient } from '$models/classes/redis-client.class';
 
-export default connectionString ? new Redis(connectionString) : new Redis();
+const REDIS_CONNECTION_STRING = process.env['DISCOVER_REDIS_CONNECTION'];
+
+const REDIS_OPTIONS: Redis.RedisOptions = {
+	retryStrategy: (times: number) => {
+		if (times > 0) {
+			return null;
+		} else {
+			return 50;
+		}
+	},
+	connectTimeout: 500,
+};
+
+export default REDIS_CONNECTION_STRING && REDIS_CONNECTION_STRING !== undefined
+	? new RedisClient(new Redis(REDIS_CONNECTION_STRING, REDIS_OPTIONS))
+	: new RedisClient(new Redis(REDIS_OPTIONS));
