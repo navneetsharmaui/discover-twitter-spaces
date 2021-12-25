324a0889b8c40a3d0c72c2d30235f553dffe6929
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-12-25 14:43:51 +0530

    Updated the service worker

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /// <reference lib="webworker" />
 
 import { build, files, timestamp } from '$service-worker';
 
-const cacheName = `DISCOVER-TWITTER-SPACES-${timestamp}`;
-const worker = window.self as unknown as ServiceWorkerGlobalScope;
+const cacheName = `DISCOVER-TWITTER-SPACE-${timestamp}`;
+const worker = self as unknown as ServiceWorkerGlobalScope;
 
 const filesToCache = build.concat(files);
 const cacheFilesWitTimeStamp = filesToCache.map((file) => `${file}?${timestamp}`);
@@ -21,16 +23,15 @@ const clear = async () => {
 	await cachedItems.delete(cacheName);
 };
 
-worker.addEventListener('install', (event: ExtendableEvent) => {
+worker.addEventListener('install', async (event: ExtendableEvent) => {
 	event.waitUntil(cache());
-	worker.skipWaiting().catch(() => {});
+	await worker.skipWaiting();
 });
 
-worker.addEventListener('activate', (event: ExtendableEvent) => {
+worker.addEventListener('activate', async (event: ExtendableEvent) => {
 	event.waitUntil(clear());
-	worker.clients.claim().catch(() => {});
+	await worker.clients.claim();
 });
-
 /**
  * Fetch the asset from the cache or the network and store it in the cache.
  * Fall back to the cache if the application is offline.
@@ -54,18 +55,16 @@ worker.addEventListener('fetch', (event) => {
 	if (event.request.method === 'GET' || event.request.headers.has('range')) return;
 
 	const url = new URL(event.request.url);
-
 	const isHttp = url.protocol.startsWith('http');
 	const isDevServerRequest =
 		url.hostname === worker.location.hostname && url.port === worker.location.port;
 	const isStaticAsset = url.host === worker.location.host && staticAssests.has(url.pathname);
 	const skipBecauseUncached = event.request.cache === 'only-if-cached' && !isStaticAsset;
-
 	if (isHttp && !isDevServerRequest && !isStaticAsset && !skipBecauseUncached) {
 		event.respondWith(
 			(async () => {
 				const cachedAsset = isStaticAsset && (await caches.match(event.request));
-				return cachedAsset || fetchAndCache(event);
+				return cachedAsset || (await fetchAndCache(event));
 			})(),
 		);
 	}
