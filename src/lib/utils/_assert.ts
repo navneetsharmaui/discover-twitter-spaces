51f41547423daa79b018d571bebe8a6ad65b9a64
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-01-13 18:09:58 +0530

    Updated the assert helper function

--- src/lib/utils/_assert.ts
+++ src/lib/utils/_assert.ts
@@ -2,11 +2,11 @@
  * This function checks if the object of given type is null or undefined. If so, it throws an error.
  * It asserts that the object is not null or undefined for functions that accepts the optional or partial values.
  */
-export const assertHelper = <T>(
+export const assertHelper = <T, U extends () => void>(
 	condition: T,
-	message: string,
+	callback: U,
 ): asserts condition is Exclude<T, null | undefined> => {
 	if (condition === null || condition === undefined) {
-		throw new Error(message);
+		callback();
 	}
 };
