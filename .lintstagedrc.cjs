6e956966572d8b102d8064088c828c82143ffcbc
Navneet Sharma <navneetnnavneet1@gmail.com>
2022-03-12 21:03:03 +0530

    Updated the lintstagedrc

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,5 +3,5 @@
 module.exports = {
 	'*.{js,ts,json,svelte}': ['yarn run format', 'git add'],
 	'./src/**/*.{svelte,ts}': ['yarn run format', 'yarn run lint:fix', 'git add'],
-	'./.{ts,cjs,js}': ['yarn run lint:fix', 'git add'],
+	'./.{ts,cjs,js}': ['yarn run format', 'yarn run lint:fix', 'git add'],
 };
